df862c2c23bb49c8db4b91ee1a0f4e4760936934
KONAN KOUAME ETIENNE <92381617+etiennekkonan@users.noreply.github.com>
2021-10-13 13:05:22 +0000

    Update settings.py

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=fzsy^eb)i=0brbcur^v_xd_adok@515)z!q*)koh@usxnd58'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
